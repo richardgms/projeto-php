e6225fdd126e5f0f4d0516a8215db911e085bd1b
Richard <richardgms001@gmail.com>
2024-11-24 19:44:08 -0300

    Adionado ícones aos elementos do menu e estilização do botão.

--- main.php
+++ main.php
@@ -19,47 +19,80 @@
             <section id="tasks">
                 <h2>Tarefas</h2>
                 <ul>
-                    <li><a href="#upcoming">Próximas</a></li>
-                    <li><a href="#today">Hoje</a></li>
-                    <li><a href="#calendar">Calendário</a></li>
-                    <li><a href="#sticky-wall">Mural</a></li>
+                    <li>
+                        <img src="./assets/double-arrow.svg" alt="Ícone" class="icon">
+                        <a href="#upcoming">Próximas</a>
+                    </li>
+                    <li>
+                        <img src="./assets/list.svg" alt="Ícone" class="icon">
+                        <a href="#today">Hoje</a>
+                    </li>
+                    <li>
+                        <img src="./assets/calendar.svg" alt="Ícone" class="icon">
+                        <a href="#calendar">Calendário</a>
+                    </li>
+                    <li>
+                        <img src="./assets/sticky-note.svg" alt="Ícone" class="icon">
+                        <a href="#sticky-wall">Mural</a>
+                    </li>
                 </ul>
             </section>
+
 
             <hr class="divider">
 
             <section id="list">
                 <h2>Listas</h2>
                 <ul>
-                    <li><a href="#personal">Pessoal</a></li>
-                    <li><a href="#work">Trabalho</a></li>
-                    <!-- Botão para adicionar nova lista -->
+                    <li>
+                        <img src="./assets/square.svg" alt="Ícone" class="icon">
+                        <a href="#personal">Pessoal</a>
+                    </li>
+                    <li>
+                        <img src="./assets/square2.svg" alt="Ícone" class="icon">
+                        <a href="#work">Trabalho</a>
+                    </li>
                     <li class="add-new">
-                        <a href="#add-new-list">+ Adicionar Nova Lista</a>
+                        <img src="./assets/add.svg" alt="Ícone de adicionar" class="icon">
+                        <a href="#add-new-list">Adicionar Nova Lista</a>
                     </li>
                 </ul>
             </section>
 
+
             <section id="others">
                 <ul>
-                    <li><a href="#settings">Configurações</a></li>
-                    <li><a href="#signout">Sair</a></li>
+                    <li>
+                        <img src="./assets/configuration.svg" alt="Ícone de configurações" class="icon">
+                        <a href="#settings">Configurações</a>
+                    </li>
+                    <li>
+                        <img src="./assets/logout.svg" alt="Ícone de sair" class="icon">
+                        <a href="#signout">Sair</a>
+                    </li>
                 </ul>
             </section>
+
 
         </section>
 
         
 
         <div id="content">
-            <h1>Hoje</h1>
-            <h2>
-                <?php
-                echo date("d");
-                ?>
-            </h2>
 
-            <button id="add-task">+ Adicionar Nova Tarefa</button>
+            <div id="content-header">
+                <h1>Hoje</h1>
+                <h2>
+                    <?php
+                    echo date("d");
+                    ?>
+                </h2>
+            </div>
+
+            <div id="button-new-task">
+                <button id="add-task">+ Adicionar Nova Tarefa</button>
+            </div>
+
         </div>
 
     </main>
