a353ed475c632b191384fc9e3446ddd65a202891
brenafigueiredo <brenaafi@gmail.com>
2024-11-25 16:03:16 -0300

    finalização do html na pagina de login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+</head>
+<body>
+    <div>
+        <h1>Login</h1>
+        <form action="verificar.php" method="POST">
+        <input type="text" name="email" placeholder="Email">
+            <br><br>
+            <input type="password" name="senha" placeholder="Senha">
+            <br><br>
+            <input class="inputSubmit" type="submit" name="submit" value="Enviar">
+        <br> <br>
+        <a href="cadastro.php">Cadastre-se</a>
+       
+    </form>
+    </div>
+</body>
+</html>
