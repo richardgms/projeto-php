f335ef564bbed9192867cfebee59df6c73e5c041
brenafigueiredo <brenaafi@gmail.com>
2024-11-25 16:00:33 -0300

    campo de senha aprimorado

---
+++ cadastro.php
@@ -0,0 +1,69 @@
+<?php
+
+    if(isset($_POST['submit']))
+    {
+   
+    include_once('config.php');
+
+    $nome = $_POST['nome'];
+    $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $data_nasc = $_POST['data_nasc'];
+
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha,email,telefone,data_nasc) 
+    VALUES ('$nome','$senha','$email','$telefone','$data_nasc')");
+
+    header('Location: login.php');
+}
+     
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Cadastro do Usuário</title>
+    <link rel="stylesheet" href="css/cadastro.css">
+    </style>
+</head>
+<body>
+    <div class="box">
+        <form action="cadastro.php" method="POST">
+            <fieldset>
+                <legend><b>Cadastro</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">Nome completo</label>
+                </div>
+                <br> <br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">Email</label>
+                </div>
+                <br><br>
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="telefone" class="labelInput">Telefone</label>
+                </div>
+                <br> <br>
+                <label for="data_nasc"><b>Data de Nascimento:</b></label>
+                <input type="date" name="data_nasc" id="data_nasc" required>
+                <br><br><br>
+                
+                <input type="submit" name="submit" id="submit">
+                <br> <br>
+                <a href="login.php">Login</a>
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
